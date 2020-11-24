08df65ad6635e682a56b7633263dc5d72f15fc4b
Daniel-Saneel <daniel.saneel@sjsu.edu>
2020-11-23 16:21:49 -0800

    'image split working'

--- upload.php
+++ upload.php
@@ -1,49 +1,102 @@
 <?php
+include('index.php');
+
+require __DIR__ . '/vendor/autoload.php';
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 $target_dir = "src/resources/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
+$logger = new Logger('main');
+$logger->pushHandler(new StreamHandler($target_dir.'jigsaw.log', Logger::DEBUG));
+
 // Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  // print_r($_FILES);
+  $check = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    $logger->info("File is an image - " . $check["mime"] . ".\n\r");
+    echo "File is an image - " . $check["mime"] . ".\n\r";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    $logger->info("File is not an image.\n\r");
+    echo "File is not an image.\n\r";
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+    $logger->info("Sorry, file already exists.\n\r");
+    echo "Sorry, file already exists.\n\r";
   $uploadOk = 0;
 }
 
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+if ($_FILES["uploadedFile"]["size"] > 500000) {
+    $logger->info("Sorry, your file is too large.\n\r");
+    echo "Sorry, your file is too large.\n\r";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
+// echo('hi' . $imageFileType);
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $logger->info("Sorry, only JPG, JPEG, PNG & GIF files are allowed.\n\r");
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.\n\r";
   $uploadOk = 0;
 }
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+      $logger->info("Sorry, your file was not uploaded.\n\r");
+      echo "Sorry, your file was not uploaded.\n\r";
+  // if everything is ok, try to upload file
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    if (move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $target_file)) {
+      $logger->info("The file ". htmlspecialchars( basename( $_FILES["uploadedFile"]["name"])). " has been uploaded.\n\r");
+      // echo "The file ". htmlspecialchars( basename( $_FILES["uploadedFile"]["name"])). " has been uploaded.\n\r";
+
+
+      $filename = 'active_image.jpg';
+
+      $width = 360; 
+      $height = 360; 
+        
+      // Get image dimensions 
+      list($width_orig, $height_orig) = getimagesize($target_file); 
+        
+      // Resample the image 
+      $image_p = imagecreatetruecolor($width, $height); 
+      $image = imagecreatefromjpeg($target_file); 
+      imagecopyresized($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig); 
+        
+      imagejpeg($image_p,$target_dir.$filename, 100);
+      
+      GenerateImages($target_dir,$filename);
+
+} else {
+    $logger->info("Sorry, there was an error uploading your file.\n\r");
+    echo "Sorry, there was an error uploading your file.\n\r";
   }
 }
+
+function GenerateImages($target_dir,$FileName){
+$viewLoad ="";
+  $im = imagecreatefromjpeg($target_dir.$FileName);
+
+  for($j = 0; $j<=2; $j++){
+    for($i = 0; $i<=2; $i++){
+    $im2 = imagecrop($im, ['x' => 120*$i, 'y' => 120*$j, 'width' => 120, 'height' => 120]);
+    imagejpeg($im2,$target_dir.$i.$j.'.jpg', 100);
+$viewLoad = $viewLoad."<div style='background-image:url(".$target_dir.$i.$j.'.jpg'.");height:120px;width:120px;display:inline-block;border:1px solid black;'></div>&nbsp;";
+  }
+  $viewLoad = $viewLoad."<br />";
+}
+echo($viewLoad);
+}
+
 ?>
